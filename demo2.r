46907ce98ebcac79133ebc72995b39eff393c986
blenz2 <blenz2@uic.edu>
2025-06-26 15:37:48 -0500

    added code

--- demo2.r
+++ demo2.r
@@ -1,1 +1,16 @@
-demo
+# demo from harvard youtube watched 06/26/25
+
+fibonacci <- function(n) {
+    c(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 
+      987, 1597, 2584, 4181)[n]
+}
+
+library(assertthat)
+assert_that(fibonacci(1) == 0)
+assert_that(fibonacci(2) == 1)
+
+for (n in 3:20) {
+    assert_that(fibonacci(n) == fibonacci(n-1) + fibonacci(n-2))
+}
+
+# no errors on this test means it passed! 
